47e572b54397882193c19d2491a82d8c98da3b7c
gaurav.nimkande <gaurav.vnimkande@angelbroking.com>
2021-07-13 17:14:59 +0530

    g

---
+++ test-output-thread/report.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "41f8e3bf-daa3-450f-bec6-8bf6c362f0ed",
+    "feature": "Login Page Feature",
+    "scenario": "Login with correct credentials",
+    "start": 1624941819676,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1624941833328,
+    "className": "passed"
+  },
+  {
+    "id": "555fc2fe-acaf-4649-bae4-c827777afc54",
+    "feature": "Login Page Feature",
+    "scenario": "forget password link",
+    "start": 1624941811167,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1624941819663,
+    "className": "passed"
+  },
+  {
+    "id": "14d2b5f1-dfc4-41ff-980b-aef899541ed3",
+    "feature": "Login Page Feature",
+    "scenario": "Login Page Title",
+    "start": 1624941798988,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1624941811147,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
